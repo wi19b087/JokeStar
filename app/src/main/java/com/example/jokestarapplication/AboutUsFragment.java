4a751e4865e21071fb4201ada56975a814450576
Daniel Arnauer <daniel.arnauer@accenture.com>
2021-06-13 16:10:25 +0200

    Fix about-us fragment

--- app/src/main/java/com/example/jokestarapplication/AboutUsFragment.java
+++ app/src/main/java/com/example/jokestarapplication/AboutUsFragment.java
@@ -67,7 +67,7 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_log_in_out, container, false);
+        View v = inflater.inflate(R.layout.fragment_about_us, container, false);
 
         return v;
     }
