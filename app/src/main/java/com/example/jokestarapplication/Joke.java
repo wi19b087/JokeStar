f58cc447d9374061eecfdb9831fbceb00128d79e
Daniel Arnauer <daniel.arnauer@accenture.com>
2021-06-14 15:10:02 +0200

    Add sending jokes to firestore

--- app/src/main/java/com/example/jokestarapplication/Joke.java
+++ app/src/main/java/com/example/jokestarapplication/Joke.java
@@ -5,17 +5,21 @@ import java.util.Date;
 import java.util.List;
 
 public class Joke {
-    private String text;
-    private int votes;
-    private Date postedDate;
-    private String Author;
-    private List<Comment> comments;
+    public String text;
+    public int votes;
+    public Date postedDate;
+    public String author;
+    public String authorId;
+    public String category;
+    public List<Comment> comments;
 
-    public Joke(String text, Date postedDate, String author) {
+    public Joke(String text, Date postedDate,String category, String author, String authorId ) {
         this.text = text;
         this.votes = 0;
         this.postedDate = postedDate;
-        Author = author;
+        this.author = author;
+        this.authorId = authorId;
+        this.category = category;
         this.comments = new ArrayList<>();
     }
 }
--- app/src/main/java/com/example/jokestarapplication/NewJokeFragment.java
+++ app/src/main/java/com/example/jokestarapplication/NewJokeFragment.java
@@ -1,11 +1,14 @@
 package com.example.jokestarapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,11 @@ import android.widget.ListAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.type.DateTime;
 
 import java.util.Calendar;
@@ -28,6 +36,8 @@ public class NewJokeFragment extends Fragment {
     private List<JokeCategory> categoryList;
     private EditText etnewJoke;
     private Button btsend;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,11 +74,32 @@ public class NewJokeFragment extends Fragment {
     }
 
     public void SendJoke(View v){
-        Joke joke = new Joke(etnewJoke.getText().toString(), Calendar.getInstance().getTime(), "TEMP"); //TODO: add author
-        getSelectedCategory(v).getJokes().add(joke);
-        Toast.makeText(this.getContext(), "Gesendet", Toast.LENGTH_LONG).show();
+        String currentCategory = getSelectedCategory(v).getName();
+        String currentUserName = mAuth.getCurrentUser().getDisplayName();
+        String currentUserId = mAuth.getCurrentUser().getUid();
+        Joke joke = new Joke(etnewJoke.getText().toString(), Calendar.getInstance().getTime(), currentCategory, currentUserName, currentUserId ); //TODO: add author
+        Log.d("SEND_JOKE", "Joke JSON: " + joke.text + " ::: " + joke.postedDate + " ::: " + joke.author );
+        addJokeToFirebase(this.getContext(), joke);
         etnewJoke.setText("");
+    }
 
-
+    private void addJokeToFirebase(Context ctx, Joke joke) {
+        // Add a new joke as a document with a generated ID to collection "Jokes"
+        db.collection("Jokes")
+                .add(joke)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("SEND_JOKE", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(ctx, "Gesendet in Kategorie " + joke.category, Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("SEND_JOKE", "Error adding document", e);
+                        Toast.makeText(ctx, "Konnte den Witz nicht posten", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
